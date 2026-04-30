6c5471d7b8a4bb5b080971c0f1a3ce38a0ea971b
LKl <plat.elena2010@ya.ru>
2026-04-30 17:14:00 +0300

    Initial

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,34 @@
+const arr = [
+  { id: 26, title: "Побег из Шоушенка", imdb: 9.30, year: 1994 },
+  { id: 25, title: "Крёстный отец", imdb: 9.20, year: 1972 },
+  { id: 27, title: "Крёстный отец 2", imdb: 9.00, year: 1974 },
+  { id: 1047, title: "Тёмный рыцарь", imdb: 9.00, year: 2008 },
+  { id: 223, title: "Криминальное чтиво", imdb: 8.90, year: 1994 }
+];
+
+const tbody = document.getElementById('movies');
+
+// 1. Создаём строки
+arr.forEach((item) => {
+  tbody.innerHTML += `
+    <tr 
+      data-id="${item.id}" 
+      data-title="${item.title}" 
+      data-year="${item.year}" 
+      data-imdb="${item.imdb}"
+    >
+      <td>${item.id}</td>
+      <td>${item.title}</td>
+      <td>(${item.year})</td>
+      <td>imdb: ${item.imdb.toFixed(2)}</td>
+    </tr>
+  `;
+});
+
+
 const trs = Array.from(document.querySelectorAll('#movies tr'));
 const ths = Array.from(document.getElementsByTagName('th'));
+
 let mode = 0;
 
 setInterval(() => {
